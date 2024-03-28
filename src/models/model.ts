1d8794ed76c065ef5c214fa50e09cfcad3d4819f
Gabriel <gabrielsougri@gmail.com>
2024-03-28 20:10:59 -0300

    add functions no arquivo model.ts

--- src/models/model.ts
+++ src/models/model.ts
@@ -4,11 +4,25 @@ import DBConnection from "../instances/mysql";
 export default class model{
     protected static sequelize: Sequelize = DBConnection.connection()
 
-    protected static atribuirId(){
+    protected static attrId(){
         return {
             primaryKey: true,
             type: DataTypes.INTEGER,
             autoIncremment: true
+        }
+    }
+
+    protected static attrEmail(){
+        return {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }
+
+    protected static attrSenha(){
+        return {
+            type: DataTypes.STRING,
+            allowNull: false
         }
     }
 }
